41cfa3bc6d4b3217875509cf6a2a07a61ed5aa68
wns5778 <75237184+wns5778@users.noreply.github.com>
2020-11-30 16:54:41 +0900

    Add files via upload

---
+++ test.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String a = "I Love You.";
+		
+		if(a.contains("Lovte")) {
+			
+			System.out.println("Me Too");
+		}
+		else {
+			System.out.println("I Hate You");
+		}
+	}
+
+}
